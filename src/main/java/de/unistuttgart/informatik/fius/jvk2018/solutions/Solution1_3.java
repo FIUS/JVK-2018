077e724350cb714090c01b7a7815ea5695b33d50
Fabian Bühler <st16290@stud.uni-stuttgart.de>
2018-10-02 21:29:30 +0200

    fix missing import

--- src/main/java/de/unistuttgart/informatik/fius/jvk2018/solutions/Solution1_3.java
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2018/solutions/Solution1_3.java
@@ -1,27 +1,29 @@
 /*
  * This source file is part of the FIUS JVK 2018 project.
  * For more information see github.com/neumantm/ICGE
- * 
+ *
  * Copyright (c) 2018 the JVK 2018 project authors.
  */
 
 package de.unistuttgart.informatik.fius.jvk2018.solutions;
 
+import de.unistuttgart.informatik.fius.jvk2018.tasks.Task1_3;
+
 /**
  * TODO: Description
- * 
+ *
  * @author
  */
 public class Solution1_3 extends Task1_3{
-    
+
     public void solve() {
         try {
             this.mario.spawn(5, 1);
-            
+
         } catch (Exception e) {
             //Todo print line for exception
         }
-        
+
     }
-    
+
 }
