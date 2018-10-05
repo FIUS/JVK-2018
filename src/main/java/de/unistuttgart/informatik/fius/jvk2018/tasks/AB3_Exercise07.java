f9222e6f257c829cd8d2088f35352994b73dfabc
TheMrPhantom <TheMrPhantom@users.noreply.github.com>
2018-10-05 17:18:06 +0200

    Added Exercise AB3_07

---
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2018/tasks/AB3_Exercise07.java
@@ -0,0 +1,48 @@
+/*
+ * This source file is part of the FIUS JVK 2018 project.
+ * For more information see github.com/neumantm/ICGE
+ * 
+ * Copyright (c) 2018 the JVK 2018 project authors.
+ */
+
+package de.unistuttgart.informatik.fius.jvk2018.tasks;
+
+import java.util.Random;
+
+import de.unistuttgart.informatik.fius.icge.course.Presets;
+import de.unistuttgart.informatik.fius.icge.course.TaskTemplate;
+import de.unistuttgart.informatik.fius.icge.simulation.Coin;
+
+/**
+ * Exercise AB3_7
+ * 
+ * @author schieljn
+ */
+public abstract class AB3_Exercise07 extends TaskTemplate {
+    
+    /**
+     * Initializes the exercise
+     */
+    public AB3_Exercise07() {
+        super(Presets.cage(3, 10).result(), "Selection sort");
+        Random rdm = new Random();
+        for (int i = 0; i < 10; i++) {
+            int coins = rdm.nextInt(10);
+            coins++;
+            for (int j = 0; j < coins; j++) {
+                new Coin(this.simulation).spawn(i, 0);
+            }
+        }
+    }
+    
+    
+    /**
+     * @see de.unistuttgart.informatik.fius.icge.course.TaskTemplate#test()
+     */
+    @Override
+    public void test() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
