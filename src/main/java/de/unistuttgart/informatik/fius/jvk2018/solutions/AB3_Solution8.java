60586a4f2d717c39afb4fe56e3ac399aecc8244b
TheMrPhantom <TheMrPhantom@users.noreply.github.com>
2018-10-07 03:48:29 +0200

    Added AB3_Exercise8

---
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2018/solutions/AB3_Solution8.java
@@ -0,0 +1,59 @@
+/*
+ * This source file is part of the FIUS JVK 2018 project.
+ * For more information see github.com/neumantm/ICGE
+ * 
+ * Copyright (c) 2018 the JVK 2018 project authors.
+ */
+
+package de.unistuttgart.informatik.fius.jvk2018.solutions;
+
+import de.unistuttgart.informatik.fius.icge.simulation.EntityType;
+import de.unistuttgart.informatik.fius.jvk2018.tasks.AB3_Exercise08;
+
+/**
+ * The solution for the Exercise AB3_Exercise07
+ * 
+ * @author schieljn
+ */
+public class AB3_Solution8 extends AB3_Exercise08 {
+    
+    /**
+     * @see de.unistuttgart.informatik.fius.icge.course.TaskTemplate#solve()
+     */
+    @Override
+    public void solve() {
+        while (this.mario.tryCollect())
+            ;
+        for (int rows = 0; rows < 4; rows++) {
+            int mod = 2;
+            for (int x = 0; x < 7; x++) {
+                if (x % mod == 0) {
+                    this.mario.tryDrop(EntityType.COIN);
+                    
+                }
+                this.mario.move();
+            }
+            this.mario.turnLeft();
+            this.mario.turnLeft();
+            this.mario.turnLeft();
+            this.mario.move();
+            this.mario.turnLeft();
+            this.mario.turnLeft();
+            this.mario.turnLeft();
+            
+            for (int x = 0; x < 7; x++) {
+                if (x % mod == 0) {
+                    this.mario.tryDrop(EntityType.COIN);
+                    
+                }
+                this.mario.move();
+            }
+            if (rows < 3) {
+                this.mario.turnLeft();
+                this.mario.move();
+                this.mario.turnLeft();
+            }
+        }
+    }
+    
+}
---
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2018/tasks/AB3_Exercise08.java
@@ -0,0 +1,50 @@
+/*
+ * This source file is part of the FIUS JVK 2018 project.
+ * For more information see github.com/neumantm/ICGE
+ * 
+ * Copyright (c) 2018 the JVK 2018 project authors.
+ */
+
+package de.unistuttgart.informatik.fius.jvk2018.tasks;
+
+import de.unistuttgart.informatik.fius.icge.course.Presets;
+import de.unistuttgart.informatik.fius.icge.course.TaskTemplate;
+import de.unistuttgart.informatik.fius.icge.simulation.Coin;
+import de.unistuttgart.informatik.fius.icge.simulation.Mario;
+
+/**
+ * Exercise AB3_7
+ * 
+ * @author schieljn
+ */
+public abstract class AB3_Exercise08 extends TaskTemplate {
+    
+    /**
+     * Mario to move
+     */
+    public final Mario mario;
+    
+    /**
+     * Initializes the exercise
+     */
+    public AB3_Exercise08() {
+        super(Presets.cage(8, 8).result(), "Chess");
+        
+        this.mario = new Mario(this.simulation);
+        this.mario.spawn(0, 0);
+        for (int i = 0; i < 32; i++) {
+            new Coin(this.simulation).spawn(0, 0);
+        }
+        
+    }
+    
+    /**
+     * @see de.unistuttgart.informatik.fius.icge.course.TaskTemplate#test()
+     */
+    @Override
+    public void test() {
+        this.solve();
+        
+    }
+    
+}
