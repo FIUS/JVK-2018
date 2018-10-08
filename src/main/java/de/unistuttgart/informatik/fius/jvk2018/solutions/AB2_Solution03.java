74131cc93baf633e45b499e2ec4378e9b701cf70
ProbstenHias <matthias.weilinger@gmx.de>
2018-10-08 23:37:06 +0200

    Wichtige Kommentare Hinzugefuegt

--- src/main/java/de/unistuttgart/informatik/fius/jvk2018/solutions/AB2_Solution03.java
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2018/solutions/AB2_Solution03.java
@@ -21,6 +21,7 @@ public class AB2_Solution03 extends AB2_Exercise03{
     @Override
     protected void stepLeftDown() {
         // TODO work here
+        //Beachte auch den Fall dass es nicht möglich ist sich in diese Richtung zu bewegen
         
     }
 
@@ -30,6 +31,7 @@ public class AB2_Solution03 extends AB2_Exercise03{
     @Override
     protected void stepBack() {
         // TODO work here
+        //Beachte auch den Fall dass es nicht möglich ist sich in diese Richtung zu bewegen
         
     }
 
@@ -39,6 +41,7 @@ public class AB2_Solution03 extends AB2_Exercise03{
     @Override
     protected void stepRightUp() {
         // TODO work here
+        //Beachte auch den Fall dass es nicht möglich ist sich in diese Richtung zu bewegen
         
     }
 }
