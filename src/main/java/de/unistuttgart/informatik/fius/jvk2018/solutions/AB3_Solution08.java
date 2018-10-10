a2b5ba367e5d326081c139953d2fb9a788ec5cba
haslersn <sebastian.hasler@gmx.net>
2018-10-10 14:06:14 +0200

    2018/AB3/Task8: remove solution

--- src/main/java/de/unistuttgart/informatik/fius/jvk2018/solutions/AB3_Solution08.java
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2018/solutions/AB3_Solution08.java
@@ -9,39 +9,19 @@ import de.unistuttgart.informatik.fius.jvk2018.tasks.AB3_Task08;
  * Solution class for worksheet 3, task 7
  */
 public class AB3_Solution08 extends AB3_Task08 {
-
-
+    
     @Override
     public void solve() {
-        Mario luigi = new Mario(this.simulation); // TODO: Use a Luigi object once the Luigi class is added to ICGE
-        luigi.spawn(0, 1);
-        int marioCount = 0;
-        int luigiCount = 0;
-        while (this.mario.canMove() && luigi.canMove()) {
-            marioCount += dice();
-            luigiCount += dice();
-            if (marioCount >= 18) {
-                this.mario.move();
-                marioCount = 0;
-            }
-            if (luigiCount >= 18) {
-                luigi.move();
-                luigiCount = 0;
-            }
-        }
-        if (this.mario.canMove()) {
-            luigi.print("LUIGI HAT GEWONNEN");
-        } else {
-            this.mario.print("MARIO HAT GEWONNEN");
-        }
+        // TODO work here (b)
     }
-
+    
     /**
      * standard dice should be implemented here
      *
      * @return random integer value from 1 to 6
      */
     public int dice() {
-        return 1 + new Random().nextInt(6);
+        // TODO work here (a)
+        return 0;
     }
 }
